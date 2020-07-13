1c99849c3a5c7bfde3a0a626be7556c5c19595ff
konoashi <64316846+konoashi@users.noreply.github.com>
2020-07-13 21:40:48 +0200

    ELECTRON

---
+++ main.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Cree la fenetre du navigateur.
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    fullscreen: true,
+    icon:'icone2.png',
+    frame: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // et charger le fichier index.html de l'application.
+  win.loadFile('index.html')
+
+  // Ouvre les DevTools.
+  
+  mainWindow.setMenu(null)
+
+
+ 
+}
+
+
+// Cette méthode sera appelée quant Electron aura fini
+// de s'initialiser et prêt à créer des fenêtres de navigation.
+// Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
+app.whenReady().then(createWindow)
+
+// Quitter si toutes les fenêtres ont été fermées.
+app.on('window-all-closed', () => {
+  // Sur macOS, il est commun pour une application et leur barre de menu
+  // de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // Sur macOS, il est commun de re-créer une fenêtre de l'application quand
+  // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// Dans ce fichier, vous pouvez inclure le reste de votre code spécifique au processus principal. Vous pouvez également le mettre dans des fichiers séparés et les inclure ici.
+
+
+
+
