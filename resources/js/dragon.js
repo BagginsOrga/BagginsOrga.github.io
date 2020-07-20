c1e25546e06e569a795075169405a91f7eb84936
konoashi <64316846+konoashi@users.noreply.github.com>
2020-07-20 16:17:04 +0200

    Trying to make a loot generator but its dificult and verrrry long

--- resources/js/dragon.js
+++ resources/js/dragon.js
@@ -10,10 +10,28 @@
 
     //tt le programme
 
+    lastbeforeinsert = document.getElementsByClassName('stat_content')
+   newdiv = document.createElement('lol')
+
+   newdiv.textContent = 'tgbgrvfegfvcdabybdchzyg'
+
    }
 
 
 
+   const lootshow = document.createElement
+   const statsContent = document.querySelector('#stats_content');
+   const itemName = statsContent.querySelector('.item-name');
+   const itemIcon = itemName.querySelector('div:first-child');
+   const itemNameContent = itemName.querySelector('span');
+   const itemLore = statsContent.querySelector('.item-lore');
+   const backpackContents = statsContent.querySelector('.backpack-contents');
 
 
-   
+   lastbeforeinsert = document.getElementsByClassName('stat_content')
+   newdiv = document.createElement('lol')
+
+   newdiv.textContent = 'tgbgrvfegfvcdabybdchzyg'
+    
+
+  
