75030d7bce321676d35b536387d6efbf2279da72
konoashi <64316846+konoashi@users.noreply.github.com>
2021-02-03 22:46:10 +0100

    Added 2 out of 4 perks (only the png) and started the div displaying system

--- resources/js/DwarvenJs.js
+++ resources/js/DwarvenJs.js
@@ -1,3 +1,19 @@
+
+function hideviv() {
+    var cacherhealth = document.getElementById("basic_stats_health");
+    var cacherdef = document.getElementById("basic_stats_def");
+    var cacherstr = document.getElementById("basic_stats_str");
+    var cacherspeed = document.getElementById("basic_stats_speed");
+    var cachercrit = document.getElementById("basic_stats_crit");
+    cacherhealth.style.display = "none";
+    cacherdef.style.display = "none";
+    cacherstr.style.display = "none";
+    cacherspeed.style.display = "none";
+    cachercrit.style.display = "none"
+}
+
+
+
 function clickhealth() {
 
     var cacherhealth = document.getElementById("basic_stats_health");
