756a8af19049b6d898454f02b63395df281f2d78
konoashi <64316846+konoashi@users.noreply.github.com>
2021-02-03 21:09:16 +0100

    Added the begin of the dwraven idle simulator.

---
+++ resources/js/DwarvenJs.js
@@ -0,0 +1,35 @@
+function clickhealth() {
+
+    var cacherhealth = document.getElementById("basic_stats_health");
+    var cacherdef = document.getElementById("basic_stats_def");
+    var cacherstr = document.getElementById("basic_stats_str");
+    var cacherspeed = document.getElementById("basic_stats_speed");
+    var cachercrit = document.getElementById("basic_stats_crit");
+
+
+    if (cacherhealth.style.display === "none") {
+
+        cacherhealth.style.display = "block";
+
+        cacherdef.style.display = "none";
+
+        cacherstr.style.display = "none";
+
+        cacherspeed.style.display = "none";
+
+        cachercrit.style.display = "none"
+
+
+
+
+
+    } else {
+        cacherhealth.style.display = "none";
+    }
+
+
+
+
+
+
+}
